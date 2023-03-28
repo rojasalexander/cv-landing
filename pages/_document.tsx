f5e925f56e7bf6d1da0aa00958e3a9cdd98fc03e
alexrojasgit <alxrojas2002@gmail.com>
2023-03-28 14:35:29 -0400

    Landing dev

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
+          
+      </Head>
       <body>
         <Main />
         <NextScript />
